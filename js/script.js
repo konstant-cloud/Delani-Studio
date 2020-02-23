bfe97ac18aaf90a83aa926f3a0b735d017fbdfc6
konstsant-cloud <adenhamza6301@gmail.com>
2020-02-24 00:34:51 +0300

    add the toggle function

--- js/script.js
+++ js/script.js
@@ -1,5 +1,18 @@
-(document).ready(functioin{
-
-}
- 
-)
+$(document).ready(function() {
+    $(".click1").click(function() {
+      $(".none1").toggle();
+      $(".click1").toggle();
+    });
+  });
+  $(document).ready(function() {
+    $(".click2").click(function() {
+      $(".none2").toggle();
+      $(".click2").toggle();
+    });
+  });
+  $(document).ready(function() {
+    $(".click3").click(function() {
+      $(".none").toggle();
+      $(".click3").toggle();
+    });
+  });
