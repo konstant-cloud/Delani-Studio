78566ef7ffab0aff2fadcf4e3515c29050edef22
konstsant-cloud <adenhamza6301@gmail.com>
2020-02-24 08:09:50 +0300

    add a toggle effect

--- js/script.js
+++ js/script.js
@@ -1,18 +1,26 @@
 $(document).ready(function() {
-    $(".click1").click(function() {
+    $(".tet1").click(function() {
       $(".none1").toggle();
       $(".click1").toggle();
     });
   });
   $(document).ready(function() {
-    $(".click2").click(function() {
+    $(".tet2").click(function() {
       $(".none2").toggle();
       $(".click2").toggle();
     });
   });
   $(document).ready(function() {
-    $(".click3").click(function() {
+    $(".tet3").click(function() {
       $(".none").toggle();
       $(".click3").toggle();
     });
   });
+
+
+function onclick() {
+    var name = document.getElementById("name").Value;
+    var email = document.getElementById("Email").Value;
+    var type = document.getElementById("text").Value;
+    alert("your message has been received");
+}
